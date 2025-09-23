ef64dccc4dae1f6885529f3c383070141bba007a
Raymond <rayjl2004@gmail.com>
2025-09-23 18:55:17 +1000

    Lint

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -2,7 +2,6 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-import { env } from "~/env";
 import { db } from "~/server/db";
 
 /**
